91917e31e06a4dacf3fa838dcad5f18ca72ba3bb
JiweiMma <67883465+JiweiMma@users.noreply.github.com>
2020-07-14 22:55:50 +0800

    Add files via upload

---
+++ kNN-1.py
@@ -0,0 +1,104 @@
+from numpy import shape, tile
+from numpy.ma import zeros
+#将数据的格式，改变为分类器可以接受的格式。
+#输入为文件名字符串，输出为训练样本矩阵和类标签向量。
+def filematrix(filename):
+    fr=open(filename)
+    arrayOLines=fr.readlines()  #得到了列表
+    numberOfLines=len(arrayOLines)#返回字符串，列表，字典，元组等长度
+    returnMat=zeros((numberOfLines,3)) #创建为0的数组
+    classLabelVector=[]
+    index=0
+    for line in arrayOLines:
+        line=line.strip()#用于移除字符串头尾指定的字符
+        listFromLine=line.split('\t')#将整行数据分割成元素列表
+        returnMat[index,:]=listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index+=1
+    return returnMat,classLabelVector
+
+#数据的可视化
+# -*- coding: UTF-8 -*-
+
+from matplotlib.font_manager import FontProperties
+import matplotlib.lines as mlines
+import matplotlib.pyplot as plt
+import numpy as np
+def showdatas(datingDataMat, datingLabels):
+    #设置汉字格式
+    font = FontProperties(fname=r"c:\windows\fonts\simsun.ttc", size=14)
+    #将fig画布分隔成1行1列,不共享x轴和y轴,fig画布的大小为(13,8)
+    #当nrow=2,nclos=2时,代表fig画布被分为四个区域,axs[0][0]表示第一行第一个区域
+    fig, axs = plt.subplots(nrows=2, ncols=2,sharex=False, sharey=False, figsize=(13,8))
+
+    numberOfLabels = len(datingLabels)
+    LabelsColors = []
+    for i in datingLabels:
+        if i == 1:
+            LabelsColors.append('black')
+        if i == 2:
+            LabelsColors.append('orange')
+        if i == 3:
+            LabelsColors.append('red')
+    #画出散点图,以datingDataMat矩阵的第一(飞行常客例程)、第二列(玩游戏)数据画散点数据,散点大小为15,透明度为0.5
+    axs[0][0].scatter(x=datingDataMat[:,0], y=datingDataMat[:,1], color=LabelsColors,s=15, alpha=.5)
+    #设置标题,x轴label,y轴label
+    axs0_title_text = axs[0][0].set_title(u'每年获得的飞行常客里程数与玩视频游戏所消耗时间占比',FontProperties=font)
+    axs0_xlabel_text = axs[0][0].set_xlabel(u'每年获得的飞行常客里程数',FontProperties=font)
+    axs0_ylabel_text = axs[0][0].set_ylabel(u'玩视频游戏所消耗时间占',FontProperties=font)
+    plt.setp(axs0_title_text, size=9, weight='bold', color='red')
+    plt.setp(axs0_xlabel_text, size=7, weight='bold', color='black')
+    plt.setp(axs0_ylabel_text, size=7, weight='bold', color='black')
+
+    #画出散点图,以datingDataMat矩阵的第一(飞行常客例程)、第三列(冰激凌)数据画散点数据,散点大小为15,透明度为0.5
+    axs[0][1].scatter(x=datingDataMat[:,0], y=datingDataMat[:,2], color=LabelsColors,s=15, alpha=.5)
+    #设置标题,x轴label,y轴label
+    axs1_title_text = axs[0][1].set_title(u'每年获得的飞行常客里程数与每周消费的冰激淋公升数',FontProperties=font)
+    axs1_xlabel_text = axs[0][1].set_xlabel(u'每年获得的飞行常客里程数',FontProperties=font)
+    axs1_ylabel_text = axs[0][1].set_ylabel(u'每周消费的冰激淋公升数',FontProperties=font)
+    plt.setp(axs1_title_text, size=9, weight='bold', color='red')
+    plt.setp(axs1_xlabel_text, size=7, weight='bold', color='black')
+    plt.setp(axs1_ylabel_text, size=7, weight='bold', color='black')
+
+    #画出散点图,以datingDataMat矩阵的第二(玩游戏)、第三列(冰激凌)数据画散点数据,散点大小为15,透明度为0.5
+    axs[1][0].scatter(x=datingDataMat[:,1], y=datingDataMat[:,2], color=LabelsColors,s=15, alpha=.5)
+    #设置标题,x轴label,y轴label
+    axs2_title_text = axs[1][0].set_title(u'玩视频游戏所消耗时间占比与每周消费的冰激淋公升数',FontProperties=font)
+    axs2_xlabel_text = axs[1][0].set_xlabel(u'玩视频游戏所消耗时间占比',FontProperties=font)
+    axs2_ylabel_text = axs[1][0].set_ylabel(u'每周消费的冰激淋公升数',FontProperties=font)
+    plt.setp(axs2_title_text, size=9, weight='bold', color='red')
+    plt.setp(axs2_xlabel_text, size=7, weight='bold', color='black')
+    plt.setp(axs2_ylabel_text, size=7, weight='bold', color='black')
+    #设置图例
+    didntLike = mlines.Line2D([], [], color='black', marker='.',markersize=6, label='1')
+    smallDoses = mlines.Line2D([], [], color='orange', marker='.',markersize=6, label='2')
+    largeDoses = mlines.Line2D([], [], color='red', marker='.',markersize=6, label='3')
+    # 添加进图例
+    axs[0][0].legend(handles=[didntLike, smallDoses, largeDoses])
+    axs[0][1].legend(handles=[didntLike, smallDoses, largeDoses])
+    axs[1][0].legend(handles=[didntLike, smallDoses, largeDoses])
+
+    plt.show()
+
+#归一化特征值，处理不同取值范围的特征值，将范围值处理为0到1或者-1到1之间
+def autoNorm(dataSet):
+    minVals=dataSet.min(0) #取每列的最小值
+    maxVals=dataSet.max(0)
+    ranges=maxVals-minVals
+    normDataSet=zeros(shape(dataSet))#待归一化的新特征矩阵
+    m=dataSet.shape[0]#样本总数
+    normDataSet=dataSet-tile(minVals,(m,1))#tile函数将变量内容复制成输入矩阵同样大小的矩阵
+    normDataSet=normDataSet/tile(ranges,(m,1))
+    return normDataSet,ranges,minVals
+
+
+if __name__=='__main__':
+    filename="DataTestSet2.txt"
+    datingDataMat,datingLabels=filematrix(filename)
+    normDataSet,ranges,minVals=autoNorm(datingDataMat)
+    print(datingDataMat)
+    print(datingLabels)
+    showdatas(datingDataMat,datingLabels)
+    print(normDataSet)
+    print(ranges)
+    print(minVals)
